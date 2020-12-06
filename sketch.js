fe17d1d130e5337dc4307a4f4d57b12bcebef669
khushi123489 <70553203+khushi123489@users.noreply.github.com>
2020-12-06 10:47:51 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var car, wall;
+var speed, weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+  speed = random(55,90);    
+  weight = random(400,1500);    
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1500, 200, 60, height/2);
+  car.velocityX = speed;    
+}
+
+function draw() {
+  background(255,255,255);  
+  wall.shapeColor = color(80,80,80);
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22509;
+    if(deformation>180)
+    {
+      car.shapeColor = color(255, 0, 0);
+    }
+    if(deformation<180 && deformatoin>100)
+    {
+      car.shapeColor = color(230, 230, 0);
+    }
+    if (deformation<100)
+    {
+      car.shapeColor = color(0, 255, 0);
+    }
+  }
+  drawSprites();
+}
+function isTouching(car, wall ){
+  if(car.x - wall.x < wall.width/2 + car.width/2
+    && wall.x - car.x < wall.width/2 + car.width/2
+    && car.y - wall.y < wall.height/2 + car.height/2
+    && wall.y - car.y < wall.height/2 + car.height/2){
+      return true;
+  }
+  else{
+    return false;
+  }
+}
